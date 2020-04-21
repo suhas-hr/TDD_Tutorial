431e924049f1f3e093c71931b21bb2967a073f16
= <=>
2020-04-21 00:39:40 -0700

    feat(TDD_Tutorial):Travis CI
    Modifications
    
    [ICTRAINING-413]

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -15,11 +15,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
 application = get_wsgi_application()
 
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
